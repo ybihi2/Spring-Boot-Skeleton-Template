eeb4c08a38523c73343b7823343e0d7b1f06bead
ybihi2 <157942422+ybihi2@users.noreply.github.com>
2025-03-25 11:35:29 -0400

    Delete Sprint 1/deliverable3 directory

--- Sprint 1/deliverable3/src/main/java/com/jydoc/deliverable3/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jydoc.deliverable3.Repository;
-
-import com.jydoc.deliverable3.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
