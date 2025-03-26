bafa5257cc7bf2249d7b009bd2b9a73d4a99ca73
ybihi2 <157942422+ybihi2@users.noreply.github.com>
2025-03-26 10:10:22 -0400

    Update UserModel.java
    additional validation

--- Sprint 1/deliverable3/deliverable3/src/main/java/com/jydoc/deliverable3/Model/UserModel.java
+++ Sprint 1/deliverable3/deliverable3/src/main/java/com/jydoc/deliverable3/Model/UserModel.java
@@ -6,6 +6,9 @@ import jakarta.persistence.*;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,18 +25,32 @@ public class UserModel {
     @Column(name="userid")
     private int id;
 
+
     @Column(name="userisadmin")
     private boolean admin;
 
+    @NotBlank
+    @Size(min = 2, max = 50, message = "First name must be 2-50 characters")
+    @Pattern(regexp = "^[\\p{L}'-]+$", message = "First name can only contain letters, hyphens, and apostrophes")
     @Column(name="userfirstname")
     private String firstName;
 
+    @NotBlank
+    @Size(min = 2, max = 50, message = "Last name must be 2-50 characters")
+    @Pattern(regexp = "^[\\p{L}'-]+$", message = "Last name can only contain letters, hyphens, and apostrophes")
     @Column(name="userlastname")
     private String lastName;
 
-    @Column(name="useremail")
+    @NotBlank
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email must be valid")
+    @Column(name="useremail", unique=true)
     private String email;
 
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d).{6,}$",
+            message = "Password must contain at least one letter and one number")
+    @NotBlank(message = "Password cannot be blank")
     @Column(name="userpassword")
     private String password;
 
